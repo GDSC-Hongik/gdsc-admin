e1c4cf67d0bfa8c0033405972e74b917020bb5f2
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-05 15:38:31 +0900

    feat: vite, typescript 절대 경로 설정 및 절대 경로 자동 변환 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  publicDir: "./public",
+  cacheDir: "./.vite",
+  resolve: {
+    alias: [
+      { find: "@apis", replacement: "/src/apis" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@constants", replacement: "/src/constants" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@libs", replacement: "/src/libs" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@stores", replacement: "/src/stores" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@types", replacement: "/src/types" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
+  },
+});
