0aa1fe3e9ce9aaff18235007283c1f911fd1337b
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-11 16:25:08 +0900

    fix: lint 관련 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
       <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
