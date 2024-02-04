ee600e7dd4ea4aac56f6300b53f930b50ef3dd32
nimod7890 <nimod1234@g.skku.edu>
2024-02-04 13:31:22 +0900

    feat: 로그인 페이지에서 token clear 단계 제거

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,12 +1,3 @@
-import useAuthStorage from "@hooks/useAuthStorage";
-import { useEffect } from "react";
-
 export default function SigninPage() {
-  const { clearToken } = useAuthStorage();
-
-  useEffect(() => {
-    clearToken();
-  }, []);
-
   return <>SigninPage</>;
 }
