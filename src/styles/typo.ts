d88b6d57d18b557c393636fd8e18bf7550b9f589
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-23 13:10:43 +0900

    feat: palette, typo, theme 파일 추가

---
+++ src/styles/typo.ts
@@ -0,0 +1,21 @@
+import { Theme } from "@emotion/react";
+import { palette } from "./palette";
+import { typo } from "./theme";
+
+export const theme: Theme = {
+  palette,
+  typo,
+};
+
+export type TypeOfPalette = typeof palette;
+export type KeyOfPalette = keyof typeof palette;
+
+export type KeyofTheme = keyof typeof theme;
+
+export type TypeOfTypo = typeof typo;
+export type KeyOfTypo = keyof typeof typo;
+
+export type TextType = {
+  typo: KeyOfTypo;
+  color: KeyOfPalette;
+};
