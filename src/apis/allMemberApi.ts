f2869bfcf04ead05e047d9a2206f5a0329412470
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-26 04:07:15 +0900

    chore: 전체 회원 조회 api 예외 처리

--- src/apis/allMemberApi.ts
+++ src/apis/allMemberApi.ts
@@ -5,12 +5,19 @@ export const allMemberApi = {
   getAllMemberList: async (
     page: number,
     size: number,
-    searchType?: string,
-    searchText?: string,
+    searchType: string,
+    searchText: string,
   ): Promise<AllMemberListDtoType> => {
-    const response = await apiClient.get(
-      `/members?${searchType}=${searchText}&page=${page}&size=${size}`,
-    );
+    if (searchText) {
+      const searchUrl = `/members?${searchType}=${searchText}&page=${page}&size=${size}`;
+
+      const response = await apiClient.get(searchUrl);
+      return response.data.content;
+    }
+
+    const commonUrl = `/members?page=${page}&size=${size}`;
+
+    const response = await apiClient.get(commonUrl);
     return response.data.content;
   },
 
