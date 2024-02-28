8750893147c41ffaa4f075f2ac9aca4d00dfdcc7
홍서현 <ghdtjgus76@naver.com>
2024-02-29 03:06:43 +0900

    [Bug/#39] 배포 사이트에서 새로 고침 시 흰 화면이 뜨는 문제 해결 (#42)
    * fix: 버셀 배포 사이트 새로 고침 시 404 뜨는 문제 해결
    
    * fix: 객체 렌더링 시 예외 처리 안 해준 부분 수정
    
    * fix: 버셀 새로고침 404 페이지 관련 에러 해결
    
    * fix: 배포 사이트에서 새로 고침 시 흰 화면 뜨는 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base: "./",
+  base: "/",
   publicDir: "./public",
   cacheDir: "./.vite",
 });
