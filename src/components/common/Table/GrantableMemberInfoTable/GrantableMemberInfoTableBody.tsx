98de569af9efb3864b32be9d7db3664d115af9de
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-29 01:24:34 +0900

    fix: dto 변경에 따른 자잘한 수정

--- src/components/common/Table/GrantableMemberInfoTable/GrantableMemberInfoTableBody.tsx
+++ src/components/common/Table/GrantableMemberInfoTable/GrantableMemberInfoTableBody.tsx
@@ -74,9 +74,9 @@ export default function GrantableMemberInfoTableBody({
       {filterTableInfo(dataList).map((row, rowIndex) => (
         <CellContainer container key={rowIndex} alignItems={"center"} height={64}>
           <Checkbox checked={checked(rowIndex)} onChange={e => handleChangeCheckbox(e, rowIndex)} />
-          {Object.entries(row).map(([key, value], index) => (
+          {Object.entries(row).map(([key, value], index) => key !== 'requirement' && (
             <TextContainer item key={index} xs={getTitleWidthRatio(key)}>
-              <Text>{formatNullableValue(value)}</Text>
+              <Text>{(value as { code: string; name: string })?.name ?? formatNullableValue(value)}</Text>
             </TextContainer>
           ))}
           <ButtonContainer>
