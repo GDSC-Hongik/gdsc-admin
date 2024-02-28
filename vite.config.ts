9283d28def72600b3e394617c35efd392bb78a4c
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-29 00:05:52 +0900

    Revert "fix: cors 해결을 위해서 vite mkcert 추가"
    This reverts commit a9ca308d01c373cf62ea364c213cfce139fea8c8.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), mkcert()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   base: "./",
   publicDir: "./public",
   cacheDir: "./.vite",
