e329b9487623ded6eef15051d2b4919211f9b089
ghdtjgus76 <ghdtjgus76@naver.com>
2024-03-05 02:17:54 +0900

    fix: 소셜 로그인으로 변경하면서 누락된 파일 추가

---
+++ src/pages/AuthSuccessRedirectPage.tsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import useAuthSuccessRedirectPage from "@/hooks/useAuthSuccessRedirectPage";
+import RoutePath from "@/routes/routePath";
+
+export default function AuthSuccessRedirectPage() {
+  const { isSuccess } = useAuthSuccessRedirectPage();
+
+  return <Navigate to={isSuccess ? RoutePath.Index : RoutePath.Signin} />;
+}
