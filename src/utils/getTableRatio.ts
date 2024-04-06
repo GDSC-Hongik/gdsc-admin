887f37b01e7e080c96c70975a02f2d1243a20f95
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-06 21:22:06 +0900

    refactor: getTableRatio 함수 리팩토링

--- src/utils/getTableRatio.ts
+++ src/utils/getTableRatio.ts
@@ -8,35 +8,24 @@ import {
 import { ManagementVariant } from "@/types/entities/member";
 import { TableRatioType } from "@/types/entities/table";
 
+const tableRatioMapping = {
+  allMember: allMemberTableWidthRatio,
+  grantedMember: allMemberTableWidthRatio,
+  grantableMember: grantableMemberTableWidthRatio,
+  paymentStatus: paymentStatusTableWidthRatio,
+  pendingMember: pendingMemberTableWidthRatio,
+};
+
 export const getTableRatio = (
   option: string,
   ratioType: TableRatioType,
   variant: ManagementVariant,
   isModal: boolean = false,
 ) => {
-  if (variant === "pendingMember" && isModal) {
-    return (
-      pendingMemberModalWidthRatio[ratioType][option] ??
-      pendingMemberModalWidthRatio[ratioType]["default"]
-    );
-  } else if (variant === "allMember" || variant === "grantedMember") {
-    return (
-      allMemberTableWidthRatio[ratioType][option] ?? allMemberTableWidthRatio[ratioType]["default"]
-    );
-  } else if (variant === "grantableMember") {
-    return (
-      grantableMemberTableWidthRatio[ratioType][option] ??
-      grantableMemberTableWidthRatio[ratioType]["default"]
-    );
-  } else if (variant === "paymentStatus") {
-    return (
-      paymentStatusTableWidthRatio[ratioType][option] ??
-      paymentStatusTableWidthRatio[ratioType]["default"]
-    );
-  } else if (variant === "pendingMember") {
-    return (
-      pendingMemberTableWidthRatio[ratioType][option] ??
-      pendingMemberTableWidthRatio[ratioType]["default"]
-    );
-  }
+  const tableRatio =
+    variant === "pendingMember" && isModal
+      ? pendingMemberModalWidthRatio
+      : tableRatioMapping[variant];
+
+  return tableRatio[ratioType][option] ?? tableRatio[ratioType]["default"];
 };
