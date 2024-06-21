97708381ff49e8fa82fa1a12944572069abee72c
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-21 17:23:12 +0900

    chore: theme, typo, palette 반영

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 import Router from "@/routes/Router";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto",
+  },
+});
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={Router} fallbackElement={null} />
-      <ReactQueryDevtools />
-      <ToastContainer />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={Router} fallbackElement={null} />
+        <ReactQueryDevtools />
+        <ToastContainer />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
