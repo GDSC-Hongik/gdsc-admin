825fc2eea640c4cb50e86d1b6d45a018c50f7d4d
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-22 01:39:17 +0900

    chore: 불필요한 로직 삭제

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import { toast } from "react-toastify";
 import {
   Button,
@@ -10,17 +10,14 @@ import {
   Stack,
   styled,
   TextField,
-  Typography,
 } from "@mui/material";
 import { ManagementVariant } from "@/types/entities/member";
 import { allMemberApi } from "@/apis/allMemberApi";
-import { SelectedMemberListContext } from "@/components/context/SelectedMemberContextProvider";
 import { commonMemberTableTitle, pendingMemberTableTitle } from "@/constants/table";
 import useGetSearchTextSetters from "@/hooks/useGetSearchTextSetters";
 import useGetSearchVariantSetters from "@/hooks/useGetSearchVariantSetters";
 import { downloadExcelFile } from "@/utils/excel";
 import { setSearchInfo } from "@/utils/setSearchInfoFunction";
-import AcceptMemberListModal from "../Modal/AcceptMemberListModal";
 
 export type HeaderProps = {
   variant: ManagementVariant;
@@ -28,9 +25,9 @@ export type HeaderProps = {
 
 export default function Header({ variant }: HeaderProps) {
   const [selectedValue, setSelectedValue] = useState("");
-  const [isAcceptModalVisible, setIsAcceptModalVisible] = useState(false);
 
-  const selectedMemberList = useContext(SelectedMemberListContext);
+  const setSearchTextFunctions = useGetSearchTextSetters();
+  const searchVariantSetters = useGetSearchVariantSetters();
 
   const handleClickExcelDownloadButton = async () => {
     try {
@@ -40,11 +37,6 @@ export default function Header({ variant }: HeaderProps) {
       toast.error("오류가 발생했습니다.");
     }
   };
-
-  const handleClickAcceptMemberButton = () => setIsAcceptModalVisible(true);
-
-  const setSearchTextFunctions = useGetSearchTextSetters();
-  const searchVariantSetters = useGetSearchVariantSetters();
 
   const handleChangeMemberSelect = (e: SelectChangeEvent<unknown>) => {
     const targetIndex = (e.target.value as number) - 1;
@@ -60,7 +52,7 @@ export default function Header({ variant }: HeaderProps) {
   };
 
   const getSelectMenuList = (variant: ManagementVariant) => {
-    if (variant === "allMember" || variant === "grantableMember" || variant === "grantedMember") {
+    if (variant === "allMember") {
       return commonMemberTableTitle;
     } else {
       return pendingMemberTableTitle.slice(0, 5);
@@ -87,29 +79,13 @@ export default function Header({ variant }: HeaderProps) {
           onChange={handleChangeText}
         />
       </StyledHeaderLeftColWrapper>
-      {variant === "allMember" && (
-        <RightColContainer>
-          <Button
-            variant="outlined"
-            onClick={handleClickExcelDownloadButton}
-            sx={{ marginRight: "20px" }}
-          >
+      <StyledHeaderRightColWrapper>
+        {variant === "allMember" && (
+          <StyledExcelDownloadButton variant="outlined" onClick={handleClickExcelDownloadButton}>
             엑셀 다운로드
-          </Button>
-        </RightColContainer>
-      )}
-      {(variant === "pendingMember" || variant === "grantableMember") && (
-        <RightColContainer>
-          <SelectedMemberCountText>{selectedMemberList.length}명 선택</SelectedMemberCountText>
-          <StyledButton variant="outlined" onClick={handleClickAcceptMemberButton}>
-            승인
-          </StyledButton>
-        </RightColContainer>
-      )}
-      <AcceptMemberListModal
-        isAcceptModalVisible={isAcceptModalVisible}
-        setIsAcceptModalVisible={setIsAcceptModalVisible}
-      />
+          </StyledExcelDownloadButton>
+        )}
+      </StyledHeaderRightColWrapper>
     </StyledHeaderWrapper>
   );
 }
@@ -130,6 +106,8 @@ const StyledHeaderLeftColWrapper = styled(Stack)({
   flexWrap: "wrap",
 });
 
+const StyledHeaderRightColWrapper = styled(Stack)({});
+
 const StyledFormWrapper = styled(FormControl)({
   width: "180px",
 });
@@ -138,17 +116,6 @@ const StyledTextField = styled(TextField)({
   width: 200,
 });
 
-const RightColContainer = styled(Stack)({
-  gap: "10px",
-  flexDirection: "row",
-  alignItems: "center",
-  flexWrap: "wrap",
-});
-
-const SelectedMemberCountText = styled(Typography)({
+const StyledExcelDownloadButton = styled(Button)({
   marginRight: "20px",
-});
-
-const StyledButton = styled(Button)({
-  marginRight: 15,
 });
--- src/pages/PendingMembersPage.tsx
+++ src/pages/PendingMembersPage.tsx
@@ -1,17 +1,14 @@
 import Header from "@/components/common/Header";
-import PendingMemberInfoTable from "@/components/common/Table/PendingMemberInfoTable";
 import Title from "@/components/common/Title";
-import SelectedMemberContextProvider from "@/components/context/SelectedMemberContextProvider";
 
 export default function PendingMembersPage() {
   return (
-    <SelectedMemberContextProvider>
+    <>
       <Title
         variant={"pendingMember"}
         descriptionText={"가입 신청서를 작성한 게스트 멤버의 정보를 관리합니다."}
       />
       <Header variant={"pendingMember"} />
-      <PendingMemberInfoTable />
-    </SelectedMemberContextProvider>
+    </>
   );
 }
