57d189e221d38793c175ae9a3fbc5a7c51b2f883
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-22 16:19:25 +0900

    refactor: 정회원 관리 테이블 컴포넌트 리팩토링

--- src/components/common/Modal/EditInfoModal/index.tsx
+++ src/components/common/Modal/EditInfoModal/index.tsx
@@ -1,186 +1,58 @@
-import { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from "react";
 import styled from "@emotion/styled";
-import { Modal, Box, Button } from "@mui/material";
-import { toast } from "react-toastify";
-import AdditionalInfo from "./AdditionalInfo";
-import BasicInfo from "./BasicInfo";
-import DiscordInfo from "./DiscordInfo";
-import useEditMemberInfoMutation from "@/hooks/mutations/useEditMemberInfoMutation";
-import { theme } from "@/styles/theme";
-import { AllMemberInfoStateType } from "@/types/entities/member";
-import { memberInfoValidation } from "@/utils/validation";
-import { formatPhoneNumber } from "@/utils/validation/formatPhoneNumber";
+import { Modal, Box, Typography } from "@mui/material";
 
-type EditInfoModalProps = {
-  isModalVisible: boolean;
-  setIsModalVisible: Dispatch<SetStateAction<boolean>>;
-  handleCloseModal: () => void;
-  selectedMemberInfo: AllMemberInfoStateType;
-  setDepartmentSearchText: Dispatch<SetStateAction<string>>;
-  departmentSearchText: string;
+export type EditMemberInfoType = {
+  memberId: number;
+  studentId: number;
+  name: string;
+  phone: string;
+  department: {
+    code: string;
+    name: string;
+  };
+  email: string;
+  discordUsername: string;
+  nickname: string;
 };
 
-export default function EditInfoModal({
-  isModalVisible,
-  setIsModalVisible,
-  handleCloseModal,
-  selectedMemberInfo,
-  setDepartmentSearchText,
-  departmentSearchText,
-}: EditInfoModalProps) {
-  const [memberInfo, setMemberInfo] = useState<AllMemberInfoStateType>({
-    name: "",
-    memberId: 0,
-    studentId: "",
-    phone: "",
-    department: {
-      code: "",
-      name: "",
-    },
-    email: "",
-    discordUsername: null,
-    nickname: null,
-  });
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+type EditInfoModalProps = {
+  open: boolean;
+  onClose: () => void;
+  memberInfo: EditMemberInfoType;
+};
 
-  const {
-    name,
-    studentId,
-    phone,
-    email,
-    discordUsername,
-    nickname,
-    memberId,
-    department: { code: departmentCode, name: departmentName },
-  } = memberInfo;
-
-  useEffect(() => {
-    setMemberInfo({
-      memberId: selectedMemberInfo.memberId || 0,
-      name: selectedMemberInfo.name || "",
-      studentId: selectedMemberInfo.studentId || "",
-      phone: selectedMemberInfo.phone || "",
-      department: selectedMemberInfo.department || {
-        code: "",
-        name: "",
-      },
-      email: selectedMemberInfo.email || "",
-      discordUsername: selectedMemberInfo.discordUsername || null,
-      nickname: selectedMemberInfo.nickname || null,
-    });
-    setDepartmentSearchText(selectedMemberInfo.department.name || "");
-  }, [selectedMemberInfo, isModalVisible, setDepartmentSearchText]);
-
-  useEffect(() => {
-    const isStudentIdValid =
-      !memberInfo.studentId ||
-      RegExp(memberInfoValidation.studentId.regExp).test(memberInfo.studentId);
-    const isPhoneValid =
-      !memberInfo.phone ||
-      RegExp(memberInfoValidation.phone.regExp).test(formatPhoneNumber(memberInfo.phone));
-    const isEmailValid =
-      !memberInfo.email || RegExp(memberInfoValidation.email.regExp).test(memberInfo.email);
-    const isNicknameValid =
-      !memberInfo.nickname ||
-      RegExp(memberInfoValidation.nickname.regExp).test(memberInfo.nickname);
-
-    const isSaveButtonDisabled = !(
-      isStudentIdValid &&
-      isPhoneValid &&
-      isEmailValid &&
-      isNicknameValid
-    );
-
-    setIsButtonDisabled(isSaveButtonDisabled);
-  }, [memberInfo]);
-
-  const handleChangeMemberInfo = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    if (name === "department") {
-      setDepartmentSearchText(value);
-    }
-
-    setMemberInfo(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleClickEditMemberInfoButton = () => {
-    if (departmentName === departmentSearchText) {
-      editMemberMutation.mutate();
-    } else {
-      toast.error("학과를 선택해주세요.");
-    }
-  };
-
-  const editMemberMutation = useEditMemberInfoMutation(
-    memberId,
-    {
-      studentId,
-      name,
-      phone: formatPhoneNumber(phone),
-      department: departmentCode,
-      email,
-      discordUsername: discordUsername || null,
-      nickname: nickname || null,
-    },
-    () => setIsModalVisible(false),
-  );
+export default function EditInfoModal({ open, onClose, memberInfo }: EditInfoModalProps) {
+  // const editMemberMutation = useEditMemberInfoMutation(
+  //   memberId,
+  //   {
+  //     studentId,
+  //     name,
+  //     phone: formatPhoneNumber(phone),
+  //     department: departmentCode,
+  //     email,
+  //     discordUsername: discordUsername || null,
+  //     nickname: nickname || null,
+  //   },
+  //   () => setIsModalVisible(false),
+  // );
 
   return (
-    <Modal open={isModalVisible} onClose={handleCloseModal}>
-      <ModalContentContainer>
-        <TitleContainer sx={{ marginBottom: "32px" }}>멤버 정보 수정</TitleContainer>
-        <BasicInfo
-          name={name}
-          studentId={studentId}
-          phone={phone}
-          handleChangeMemberInfo={handleChangeMemberInfo}
-        />
-        <AdditionalInfo
-          email={email}
-          setMemberInfo={setMemberInfo}
-          handleChangeMemberInfo={handleChangeMemberInfo}
-          departmentSearchText={departmentSearchText}
-          setDepartmentSearchText={setDepartmentSearchText}
-        />
-        <DiscordInfo
-          discordUsername={discordUsername}
-          nickname={nickname}
-          handleChangeMemberInfo={handleChangeMemberInfo}
-        />
-        <StyledButton
-          variant={"contained"}
-          size="large"
-          disabled={isButtonDisabled}
-          onClick={handleClickEditMemberInfoButton}
-        >
-          저장하기
-        </StyledButton>
-      </ModalContentContainer>
+    <Modal open={open} onClose={onClose}>
+      <StyledModalContentWrapper>
+        <Typography>{memberInfo.name}</Typography>
+      </StyledModalContentWrapper>
     </Modal>
   );
 }
 
-const ModalContentContainer = styled(Box)({
-  width: "60%",
-  height: "520px",
+const StyledModalContentWrapper = styled(Box)({
+  width: "988px",
+  height: "684px",
   backgroundColor: "white",
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  padding: "64px 63px 44px 63px",
   textAlign: "center",
-});
-
-const TitleContainer = styled(Box)({
-  ...theme.typo.title1,
-});
-
-const StyledButton = styled(Button)({
-  width: "216px",
-  height: "43px",
+  padding: "58px 64px 44px 64px",
 });
--- src/components/common/Table/AllMemberInfoTable/index.tsx
+++ src/components/common/Table/AllMemberInfoTable/index.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
-import { Button, Grid } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import { DataGrid, GridCellParams, GridColDef, GridRowModel } from "@mui/x-data-grid";
 import { useAllMembersSearchInfoState } from "@/hooks/contexts/useAllMemberSearchInfoContext";
 import useGetAllMemberListQuery from "@/hooks/queries/useGetAllMemberListQuery";
 import { AllMemberInfoType } from "@/types/entities/member";
-import styled from "@emotion/styled";
+import EditInfoModal from "../../Modal/EditInfoModal";
+import useDeleteMemberMutation from "@/hooks/mutations/useDeleteMemberMutation";
 
 export default function AllMemberInfoTable() {
   const [paginationModel, setPaginationModel] = useState({
     pageSize: 5,
     page: 0,
   });
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [editMemberInfo, setEditMemberInfo] = useState({
+    memberId: 0,
+    studentId: 0,
+    name: "",
+    phone: "",
+    department: {
+      code: "",
+      name: "",
+    },
+    email: "",
+    discordUsername: "",
+    nickname: "",
+  });
+
+  const { mutate } = useDeleteMemberMutation();
 
   const { searchText, searchVariant } = useAllMembersSearchInfoState();
 
@@ -47,23 +65,67 @@ export default function AllMemberInfoTable() {
     });
   };
 
+  const handleClickEditMemberInfo = (row: GridRowModel) => {
+    const {
+      id: memberId,
+      studentId,
+      name,
+      phone,
+      department,
+      email,
+      discordUsername,
+      nickname,
+    } = row;
+
+    setEditMemberInfo({
+      memberId,
+      studentId,
+      name,
+      phone,
+      department,
+      email,
+      discordUsername,
+      nickname,
+    });
+    setEditModalOpen(true);
+  };
+
+  const handleClickDeleteMember = (row: GridRowModel) => {
+    const targetMemberId = row.id;
+    mutate(targetMemberId);
+  };
+
+  const handleCloseModal = () => setEditModalOpen(false);
+
   return (
-    <Grid container>
+    <>
       <StyledDataGrid
         rows={getFilteredRows(allMemberList)}
-        columns={columns}
+        columns={getColumns(handleClickEditMemberInfo, handleClickDeleteMember)}
         pageSizeOptions={[5, 25, 100]}
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
         disableRowSelectionOnClick
         autoHeight
         disableColumnFilter
+        disableColumnMenu
       />
-    </Grid>
+      {editModalOpen && (
+        <EditInfoModal
+          open={editModalOpen}
+          onClose={handleCloseModal}
+          memberInfo={editMemberInfo}
+          key={editMemberInfo.memberId}
+        />
+      )}
+    </>
   );
 }
 
-const columns: GridColDef[] = [
+const getColumns = (
+  handleClickEditMemberInfo: (row: GridRowModel) => void,
+  handleClickDeleteMember: (row: GridRowModel) => void,
+): GridColDef[] => [
   {
     field: "studentId",
     headerName: "학번",
@@ -76,7 +138,7 @@ const columns: GridColDef[] = [
     field: "name",
     headerName: "이름",
     headerAlign: "center",
-    width: 60,
+    width: 75,
     resizable: false,
     editable: false,
   },
@@ -108,7 +170,7 @@ const columns: GridColDef[] = [
     field: "discordUsername",
     headerName: "디스코드 사용자명",
     headerAlign: "center",
-    width: 120,
+    width: 150,
     resizable: false,
     editable: false,
   },
@@ -116,7 +178,7 @@ const columns: GridColDef[] = [
     field: "nickname",
     headerName: "디스코드 별명",
     headerAlign: "center",
-    width: 105,
+    width: 125,
     resizable: false,
     editable: false,
   },
@@ -125,15 +187,21 @@ const columns: GridColDef[] = [
     headerName: "",
     sortable: false,
     width: 156,
-    renderCell: () => {
-      const onClick = () => {};
-
+    renderCell: (params: GridCellParams) => {
       return (
         <StyledButtonWrapper>
-          <StyledButton variant="outlined" color="primary" onClick={onClick}>
+          <StyledButton
+            variant="outlined"
+            color="primary"
+            onClick={() => handleClickEditMemberInfo(params.row)}
+          >
             수정
           </StyledButton>
-          <StyledButton variant="outlined" color="error" onClick={onClick}>
+          <StyledButton
+            variant="outlined"
+            color="error"
+            onClick={() => handleClickDeleteMember(params.row)}
+          >
             탈퇴
           </StyledButton>
         </StyledButtonWrapper>
