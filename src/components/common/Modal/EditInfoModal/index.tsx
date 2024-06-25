4d88899987f950fd23b95f2cd272ffb8b04fae74
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-25 18:00:40 +0900

    chore: 학과 정보 제외 멤버 정보 수정 기능 추가

--- src/components/common/Modal/EditInfoModal/index.tsx
+++ src/components/common/Modal/EditInfoModal/index.tsx
@@ -1,9 +1,13 @@
+import { ChangeEvent, useState } from "react";
 import styled from "@emotion/styled";
-import { Modal, Box, Typography } from "@mui/material";
+import { Modal, Box, Typography, Button, TextField } from "@mui/material";
+import { typo } from "@/styles/typo";
+import useEditMemberInfoMutation from "@/hooks/mutations/useEditMemberInfoMutation";
+import { formatPhoneNumber } from "@/utils/validation/formatPhoneNumber";
 
 export type EditMemberInfoType = {
   memberId: number;
-  studentId: number;
+  studentId: string;
   name: string;
   phone: string;
   department: {
@@ -22,36 +26,196 @@ type EditInfoModalProps = {
 };
 
 export default function EditInfoModal({ open, onClose, memberInfo }: EditInfoModalProps) {
-  // const editMemberMutation = useEditMemberInfoMutation(
-  //   memberId,
-  //   {
-  //     studentId,
-  //     name,
-  //     phone: formatPhoneNumber(phone),
-  //     department: departmentCode,
-  //     email,
-  //     discordUsername: discordUsername || null,
-  //     nickname: nickname || null,
-  //   },
-  //   () => setIsModalVisible(false),
-  // );
+  const [modalMemberInfo, setModalMemberInfo] = useState({
+    ...memberInfo,
+    discordUsername: memberInfo.discordUsername || null,
+    nickname: memberInfo.nickname || null,
+  });
+
+  const {
+    memberId,
+    studentId,
+    name,
+    phone,
+    department: { code: departmentCode },
+    email,
+    discordUsername,
+    nickname,
+  } = modalMemberInfo;
+
+  const { mutate } = useEditMemberInfoMutation(
+    memberId,
+    {
+      studentId,
+      name,
+      phone: formatPhoneNumber(phone),
+      department: departmentCode,
+      email,
+      discordUsername: discordUsername,
+      nickname: nickname,
+    },
+    () => onClose(),
+  );
+
+  const handleEditMemberInfo = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "department") {
+    }
+
+    setModalMemberInfo(prevMemberInfo => ({
+      ...prevMemberInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleClickSave = () => {
+    mutate();
+  };
 
   return (
     <Modal open={open} onClose={onClose}>
       <StyledModalContentWrapper>
-        <Typography>{memberInfo.name}</Typography>
+        <Typography css={typo.h1} sx={{ marginBottom: "38px" }}>
+          정회원 정보 수정
+        </Typography>
+        <StyledInfoContainerWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>이름</Typography>
+            <StyledTextField
+              placeholder="이름"
+              size="small"
+              value={modalMemberInfo.name}
+              name="name"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>학번</Typography>
+            <StyledTextField
+              placeholder="학번"
+              size="small"
+              value={modalMemberInfo.studentId}
+              name="studentId"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>전화번호</Typography>
+            <StyledTextField
+              placeholder="전화번호"
+              size="small"
+              value={modalMemberInfo.phone}
+              name="phone"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+        </StyledInfoContainerWrapper>
+        <StyledInfoContainerWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>소속 학과</Typography>
+            <StyledTextField
+              placeholder="학과"
+              size="small"
+              value={modalMemberInfo.department?.name}
+              name="department"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>이메일</Typography>
+            <StyledTextField
+              placeholder="이메일"
+              size="small"
+              value={modalMemberInfo.email}
+              name="email"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+        </StyledInfoContainerWrapper>
+        <StyledInfoContainerWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>디스코드 핸들명</Typography>
+            <StyledTextField
+              placeholder="핸들명"
+              size="small"
+              value={modalMemberInfo.discordUsername}
+              name="discordUsername"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+          <StyledInfoWrapper>
+            <Typography css={typo.h6}>디스코드 커뮤니티 닉네임</Typography>
+            <StyledTextField
+              placeholder="닉네임"
+              size="small"
+              value={modalMemberInfo.nickname}
+              name="nickname"
+              onChange={handleEditMemberInfo}
+            />
+          </StyledInfoWrapper>
+        </StyledInfoContainerWrapper>
+        <StyledButton size="large" variant="contained" css={typo.button1} onClick={handleClickSave}>
+          저장하기
+        </StyledButton>
       </StyledModalContentWrapper>
     </Modal>
   );
 }
 
-const StyledModalContentWrapper = styled(Box)({
-  width: "680px",
-  height: "450px",
+const StyledModalContentWrapper = styled("main")({
+  width: "988px",
+  height: "640px",
   backgroundColor: "white",
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   textAlign: "center",
+  padding: "58px",
+  boxSizing: "border-box",
+});
+
+const StyledInfoContainerWrapper = styled(Box)({
+  display: "flex",
+  gap: "19.15px",
+  height: "71px",
+  marginBottom: "48px",
+});
+
+const StyledInfoWrapper = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "12px",
+  flex: 1,
+});
+
+const StyledTextField = styled(TextField)({
+  "width": "100%",
+
+  ".MuiInputBase-root": {
+    borderRadius: 0,
+    border: "1px solid #BEC3CC",
+    padding: "8px 14px",
+    height: "40px",
+  },
+
+  ".MuiInputBase-input": {
+    padding: 0,
+    fontFamily: "Pretendard",
+    fontSize: "14px",
+    fontWeight: 500,
+    lineHeight: "22.4px",
+  },
+
+  ".MuiInputBase-input::placeholder": {
+    color: "#646D7A",
+  },
+});
+
+const StyledButton = styled(Button)({
+  width: "316px",
+  height: "63px",
+  padding: "8px 22px",
 });
