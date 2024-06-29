bd09a1e8bc964385a6f498c918e2babe6e987c9e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-29 16:43:28 +0900

    feat: 발급 쿠폰 관리 헤더 추가

--- src/pages/CouponProvisionMembersPage.tsx
+++ src/pages/CouponProvisionMembersPage.tsx
@@ -1,9 +1,11 @@
+import CouponProvisionHeader from "@/components/common/Header/CouponProvisionHeader";
 import Title from "@/components/common/Title";
 
 export default function CouponProvisionMembersPage() {
   return (
     <>
       <Title variant={"couponProvision"} descriptionText={"발급된 쿠폰 관리 설명"} />
+      <CouponProvisionHeader />
     </>
   );
 }
