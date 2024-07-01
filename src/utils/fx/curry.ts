ee17c3d949f5777c13685d3762ca5bd9f2e5a51f
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-02 02:26:36 +0900

    chore: 불필요한 파일 삭제

--- src/utils/fx/curry.ts
+++
@@ -1,8 +0,0 @@
-type Curry = <T, U>(
-  f: (arg: T, ...rest: any[]) => U,
-) => (arg: T, ...rest: any[]) => U | ((...rest: any[]) => U);
-
-export const curry: Curry =
-  f =>
-  (a, ...rest) =>
-    rest.length ? f(a, ...rest) : (...rest) => f(a, ...rest);
