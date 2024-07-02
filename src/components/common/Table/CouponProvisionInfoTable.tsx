82377f4ca0f8acf66ffbc07b44640bdd19a84eb8
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-02 14:20:03 +0900

    feat: 쿠폰 지급 페이지 상세 모달 추가

--- src/components/common/Table/CouponProvisionInfoTable.tsx
+++ src/components/common/Table/CouponProvisionInfoTable.tsx
@@ -1,52 +1,153 @@
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridCellParams, GridColDef, GridRowModel } from "@mui/x-data-grid";
+import {
+  useCouponProvisionSearchInfoDispatch,
+  useCouponProvisionSearchInfoState,
+} from "@/hooks/contexts/useCouponProvisionSearchInfoContext";
+import { useMemo, useRef, useState } from "react";
+import useGetAllMemberListQuery from "@/hooks/queries/useGetAllMemberListQuery";
+import MemberDetailInfoModal from "../Modal/MemberDetailInfoModal";
+import { DetailMemberInfoType } from "@/types/dtos/member";
 
 const mockData = [
   {
     semester: "2024-1",
-    member: {
-      memberId: 1,
-      studentId: "C111206",
-      name: "홍서현",
-      phone: "010-8712-0786",
-      discordUsername: "홍서현",
-      nickname: "홍서현",
+    memberId: 0,
+    studentId: "C111206",
+    name: "홍서현",
+    phone: "010-8712-0786",
+    department: {
+      code: "D001",
+      name: "컴퓨터공학전공",
+    },
+    email: "ghdtjgus76@naver.com",
+    discordUsername: "홍서현",
+    nickname: "홍서현",
+    requirement: {
+      univStatus: "VERIFIED",
+      discordStatus: "VERIFIED",
+      bevyStatus: "VERIFIED",
     },
   },
 ];
 
 export default function CouponProvisionInfoTable() {
+  const [detailMemberInfo, setDetailMemberInfo] = useState<DetailMemberInfoType>({
+    name: "",
+    studentId: "",
+    phone: "",
+    departmentName: "",
+    email: "",
+    discordUsername: "",
+    nickname: "",
+  });
+  const [detailModalOpen, setDetailModalOpen] = useState(false);
+
+  const { paginationModel, searchText, searchVariant } = useCouponProvisionSearchInfoState();
+  const { setPaginationModel } = useCouponProvisionSearchInfoDispatch();
+
+  const { allMemberList = [], totalElements } = useGetAllMemberListQuery(
+    paginationModel.page,
+    paginationModel.pageSize,
+    searchVariant,
+    searchText,
+  );
+
+  const rowCountRef = useRef(totalElements || 0);
+
+  const rowCount = useMemo(() => {
+    if (totalElements !== undefined) {
+      rowCountRef.current = totalElements;
+    }
+
+    return rowCountRef.current;
+  }, [totalElements]);
+
   const getFilteredMemberList = (allMemberList: any[]) => {
     return allMemberList.map(memberInfo => ({
-      id: memberInfo.member.memberId,
+      id: memberInfo.memberId,
       semester: memberInfo.semester,
-      studentId: memberInfo.member.studentId,
-      name: memberInfo.member.name,
-      phone: memberInfo.member.phone,
-      discordUsername: memberInfo.member.discordUsername,
-      nickname: memberInfo.member.nickname,
+      studentId: memberInfo.studentId,
+      name: memberInfo.name,
+      phone: memberInfo.phone,
+      discordUsername: memberInfo.discordUsername,
+      nickname: memberInfo.nickname,
+      departmentName: memberInfo.department.name,
+      email: memberInfo.email,
     }));
+  };
+
+  const getFilteredDetailInfoList = (detailMemberInfo: DetailMemberInfoType) => {
+    const { name, studentId, phone, departmentName, email, discordUsername, nickname } =
+      detailMemberInfo;
+
+    return {
+      "이름": name,
+      "학번": studentId,
+      "전화번호": phone,
+      "소속 학과": departmentName,
+      "이메일": email,
+      "디스코드 사용자명": discordUsername,
+      "디스코드 별명": nickname,
+    };
+  };
+
+  const handleClickDetail = (row: GridRowModel) => {
+    const { name, studentId, phone, departmentName, email, discordUsername, nickname } = row;
+
+    setDetailMemberInfo({
+      name,
+      studentId,
+      phone,
+      departmentName,
+      email,
+      discordUsername,
+      nickname,
+    });
+    setDetailModalOpen(true);
+  };
+
+  const handleCloseDetailModal = () => {
+    setDetailMemberInfo({
+      name: "",
+      studentId: "",
+      phone: "",
+      departmentName: "",
+      email: "",
+      discordUsername: "",
+      nickname: "",
+    });
+    setDetailModalOpen(false);
   };
 
   return (
     <>
       <StyledDataGrid
         rows={getFilteredMemberList(mockData)}
-        columns={columns}
+        columns={getColumns(handleClickDetail)}
         checkboxSelection
         autoHeight
+        pageSizeOptions={[5, 25, 100]}
+        rowCount={rowCount}
+        paginationMode="server"
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
         disableRowSelectionOnClick
         disableColumnFilter
         disableColumnMenu
         disableColumnSorting
-        hideFooterPagination
+      />
+      <MemberDetailInfoModal
+        open={detailModalOpen}
+        onClose={handleCloseDetailModal}
+        detailInfo={getFilteredDetailInfoList(detailMemberInfo)}
       />
     </>
   );
 }
 
-const columns: GridColDef[] = [
+const getColumns = (handleClickDetail: (row: GridRowModel) => void): GridColDef[] => [
   {
     field: "semester",
     headerName: "학기",
@@ -106,10 +207,14 @@ const columns: GridColDef[] = [
     headerName: "",
     sortable: false,
     flex: 1,
-    renderCell: (_: GridCellParams) => {
+    renderCell: (params: GridCellParams) => {
       return (
         <StyledButtonWrapper>
-          <StyledButton variant="outlined" color="secondary" onClick={() => {}}>
+          <StyledButton
+            variant="outlined"
+            color="secondary"
+            onClick={() => handleClickDetail(params.row)}
+          >
             상세
           </StyledButton>
         </StyledButtonWrapper>
