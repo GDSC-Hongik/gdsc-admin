8b928b596a6ab692326c1c70893cc33e63874566
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-03 15:03:28 +0900

    chore: 리크루팅 타이틀 설명 변경

--- src/pages/RecruitingPage.tsx
+++ src/pages/RecruitingPage.tsx
@@ -4,7 +4,7 @@ import Title from "@/components/common/Title";
 export default function RecruitingPage() {
   return (
     <>
-      <Title variant={"recruiting"} descriptionText={"리크루팅 관리 설명"} />
+      <Title variant={"recruiting"} descriptionText={"매 학기 신청 받을 리크루팅을 관리합니다."} />
       <RecruitingHeader />
     </>
   );
