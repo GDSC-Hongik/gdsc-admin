51f2482d7ce2c09accad3f6d50098e7a175eb0ef
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-06 18:54:08 +0900

    feat: 모집 회차 관리 페이지 타이틀 추가

--- src/pages/RecruitingRoundPage.tsx
+++ src/pages/RecruitingRoundPage.tsx
@@ -1,3 +1,12 @@
+import Title from "@/components/common/Title";
+
 export default function RecruitingRoundPage() {
-  return <>RecruitingRoundPage</>;
+  return (
+    <>
+      <Title
+        variant={"recruitingRound"}
+        descriptionText={"매 학기 신청 받을 리크루팅을 관리합니다."}
+      />
+    </>
+  );
 }
