503d8a09e27105159e9c905d47337a63b8f748e2
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-14 13:23:24 +0900

    chore: 쿠폰 검색 관련 정보 context에서 삭제

--- src/contexts/CouponSearchInfoContext.tsx
+++ src/contexts/CouponSearchInfoContext.tsx
@@ -1,48 +1,35 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
-import { SearchVariantType } from "@/types/entities/member";
 
 type CouponSearchInfoContextProviderPropsType = {
   children: ReactNode;
 };
 
 type SearchStateType = {
-  searchText: string;
-  searchVariant: SearchVariantType;
   modalOpen: boolean;
 };
 
 type SearchDispatchType = {
-  setSearchText: Dispatch<SetStateAction<string>>;
-  setSearchVariant: Dispatch<SetStateAction<SearchVariantType>>;
   setModalOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 const defaultState: SearchStateType = {
-  searchText: "",
-  searchVariant: "studentId",
   modalOpen: false,
 };
 
 export const CouponSearchInfoStateContext = createContext<SearchStateType>(defaultState);
 
 export const CouponSearchInfoDispatchContext = createContext<SearchDispatchType>({
-  setSearchText: () => {},
-  setSearchVariant: () => {},
   setModalOpen: () => {},
 });
 
 export default function CouponSearchInfoContextProvider({
   children,
 }: CouponSearchInfoContextProviderPropsType) {
-  const [searchText, setSearchText] = useState("");
-  const [searchVariant, setSearchVariant] = useState<SearchVariantType>("studentId");
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <CouponSearchInfoDispatchContext.Provider
-      value={{ setSearchText, setSearchVariant, setModalOpen }}
-    >
-      <CouponSearchInfoStateContext.Provider value={{ searchText, searchVariant, modalOpen }}>
+    <CouponSearchInfoDispatchContext.Provider value={{ setModalOpen }}>
+      <CouponSearchInfoStateContext.Provider value={{ modalOpen }}>
         {children}
       </CouponSearchInfoStateContext.Provider>
     </CouponSearchInfoDispatchContext.Provider>
