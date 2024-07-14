27df2fa03760928b10518c13a3016bf98defe61e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-14 20:42:10 +0900

    feat: 리크루팅 회차 관리 관련 타입 추가

--- src/types/entities/recruiting.ts
+++ src/types/entities/recruiting.ts
@@ -1,3 +1,5 @@
+import { Dayjs } from "dayjs";
+
 export type RecruitmentType = {
   recruitmentId: number;
   semester: string;
@@ -9,7 +11,7 @@ export type RecruitmentType = {
 
 export type SemesterVariantType = "FIRST" | "SECOND";
 
-export type RecruitmentRoundType = {
+export type RecruitmentRoundInfoType = {
   recruitmentRoundId: number;
   semester: string;
   roundType: "FIRST" | "SECOND";
@@ -17,3 +19,25 @@ export type RecruitmentRoundType = {
   endDate: string;
   name: string;
 };
+
+export type RecruitingRoundModalInfoType = {
+  recruitmentRoundId: number;
+  academicYear: string;
+  semester: string;
+  roundType: RecruitmentRoundType;
+  startDate: Dayjs | null;
+  endDate: Dayjs | null;
+  name: string;
+};
+
+export type RecruitingRoundInfoType = {
+  id: number;
+  academicYear: string;
+  roundType: RecruitmentRoundType;
+  semester: string;
+  startDate: string;
+  endDate: string;
+  name: string;
+};
+
+export type RecruitmentRoundType = "1차" | "2차";
