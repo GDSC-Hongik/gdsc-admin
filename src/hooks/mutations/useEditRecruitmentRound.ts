27ce98b8fa18bc994e34eafa7872dfa618635c60
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-14 19:38:33 +0900

    feat: 회차 정보 관리 페이지 수정 조회 api 쿼리 훅 작성

---
+++ src/hooks/mutations/useEditRecruitmentRound.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { recruitingApi } from "@/apis/recruitingApi";
+import { QueryKey } from "@/constants/queryKey";
+
+export default function useEditRecruitmentRound() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      recruitmentRoundId,
+      body,
+    }: {
+      recruitmentRoundId: number;
+      body: {
+        name: string;
+        startDate: string;
+        endDate: string;
+        roundType: "FIRST" | "SECOND";
+      };
+    }) => recruitingApi.editRecruitmentRound(recruitmentRoundId, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKey.recruitmentRound],
+      });
+      toast.success("모집 회차 정보가 수정되었습니다.");
+    },
+    onError: (error: any) => {
+      toast.error(error.response.data.errorMessage);
+    },
+  });
+}
