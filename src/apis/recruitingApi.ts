958c2a15e417b998ec8279c3804f8dad1e546b5d
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-14 18:58:20 +0900

    feat: 회차 정보 관리 페이지 정보 조회 api 함수 작성

--- src/apis/recruitingApi.ts
+++ src/apis/recruitingApi.ts
@@ -1,4 +1,5 @@
 import { apiClient } from ".";
+import { RecruitmentRoundResponseDtoType } from "@/types/dtos/recruiting";
 import { SemesterVariantType } from "@/types/entities/recruiting";
 
 export const recruitingApi = {
@@ -6,6 +7,11 @@ export const recruitingApi = {
     const response = await apiClient.patch(
       `/admin/members/demotion?academicYear=${academicYear}&semesterType=${semesterType}`,
     );
+    return response.data;
+  },
+
+  getRecruitmentsRounds: async (): Promise<RecruitmentRoundResponseDtoType> => {
+    const response = await apiClient.get("/admin/recruitments/rounds");
     return response.data;
   },
 };
