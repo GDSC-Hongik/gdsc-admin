e762361063f5a0f0ac2f6f9788c1cc3300cccc0c
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-15 22:59:39 +0900

    fix: 네이밍 변경사항 반영

---
+++ src/contexts/PaymentStatusContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { PaginationModelType, SearchInfoType } from "@/types/entities/common";
+
+type PaymentStatusContextProviderPropsType = {
+  children: ReactNode;
+};
+
+type PaymentStatusStateContextType = {
+  searchInfo: SearchInfoType;
+  paginationModel: PaginationModelType;
+};
+
+type PaymentStatusStateDispatchType = {
+  setSearchInfo: Dispatch<SetStateAction<SearchInfoType>>;
+  setPaginationModel: Dispatch<SetStateAction<PaginationModelType>>;
+};
+
+const defaultState: PaymentStatusStateContextType = {
+  searchInfo: {
+    text: "",
+    variant: "studentId",
+  },
+  paginationModel: {
+    pageSize: 5,
+    page: 0,
+  },
+};
+
+export const PaymentStatusStateContext =
+  createContext<PaymentStatusStateContextType>(defaultState);
+
+export const PaymentStatusDispatchContext = createContext<PaymentStatusStateDispatchType>(
+  {
+    setSearchInfo: () => {},
+    setPaginationModel: () => {},
+  },
+);
+
+export default function PaymentStatusContextProvider({
+  children,
+}: PaymentStatusContextProviderPropsType) {
+  const [searchInfo, setSearchInfo] = useState<SearchInfoType>(defaultState.searchInfo);
+  const [paginationModel, setPaginationModel] = useState<PaginationModelType>({
+    pageSize: 5,
+    page: 0,
+  });
+
+  return (
+    <PaymentStatusDispatchContext.Provider value={{ setSearchInfo, setPaginationModel }}>
+      <PaymentStatusStateContext.Provider value={{ searchInfo, paginationModel }}>
+        {children}
+      </PaymentStatusStateContext.Provider>
+    </PaymentStatusDispatchContext.Provider>
+  );
+}
