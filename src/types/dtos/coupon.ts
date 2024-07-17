9b549f9a2296ffdf4ce4eb2762b4afd731551379
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-17 13:47:40 +0900

    feat: 쿠폰 지급 명단 조회 api 쿼리 dto 타입 추가

--- src/types/dtos/coupon.ts
+++ src/types/dtos/coupon.ts
@@ -1,5 +1,11 @@
+import { PaginationElementType } from "./common";
 import { CouponType, IssuedCouponType } from "../entities/coupon";
+import { MemberInfoType } from "../entities/member";
 
 export type CouponListResponseDtoType = CouponType[];
 
 export type IssuedCouponListResponseDtoType = IssuedCouponType[];
+
+export type CouponProvisionMemberListResponseDtoType = {
+  content: MemberInfoType[];
+} & PaginationElementType;
