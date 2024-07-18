1e6ff7f8c641bc27610c04364100916fd6f614be
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-18 13:24:09 +0900

    feat: 모집 회차 생성 api mutation 훅 작성

---
+++ src/hooks/mutations/useCreateRecruitmentRoundMutation.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { recruitmentApi } from "@/apis/recruitmentApi";
+import { RoundVariantType, SemesterVariantType } from "@/types/entities/recruitment";
+
+type CreateRecruitmentRoundMutationArgumentType = {
+  body: {
+    academicYear: number;
+    semesterType: SemesterVariantType;
+    name: string;
+    startDate: string;
+    endDate: string;
+    roundType: RoundVariantType;
+  };
+};
+
+export default function useCreateRecruitmentRoundMutation() {
+  return useMutation({
+    mutationFn: ({ body }: CreateRecruitmentRoundMutationArgumentType) =>
+      recruitmentApi.createRecruitmentRound(body),
+    onError: (error: any) => {
+      toast.error(error.response.data.errorMessage);
+    },
+  });
+}
