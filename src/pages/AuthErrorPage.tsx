fbd11bb1dbd0e9d64ae9eaf94d72d70d17939d10
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-19 01:21:27 +0900

    refactor: AuthErrorPage 기존 컨벤션에 맞게 리팩토링

--- src/pages/AuthErrorPage.tsx
+++ src/pages/AuthErrorPage.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Stack, Box, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import RoutePath from "@/routes/routePath";
@@ -6,24 +7,30 @@ import { theme } from "@/styles/theme";
 export default function AuthErrorPage() {
   const navigation = useNavigate();
 
+  const handleClickLogin = () => navigation(RoutePath.Signin);
+
   return (
-    <Stack sx={{ height: "100vh", justifyContent: "space-between", alignItems: "center" }}>
-      <Stack sx={{ paddingTop: 20 }} spacing={4}>
-        <Box
-          sx={{
-            borderRadius: 2,
-            backgroundColor: theme.palette.gray5,
-            fontWeight: 700,
-            padding: 3,
-          }}
-          textAlign="center"
-        >
-          접근할 수 없는 유저입니다.
-          <br />
-          운영진에게 문의해주세요.
-        </Box>
-        <Button onClick={() => navigation(RoutePath.Signin)}>다시 로그인하기</Button>
-      </Stack>
-    </Stack>
+    <StyledAuthErrorPageContainer spacing={4}>
+      <StyledTextContainer textAlign="center">
+        접근할 수 없는 유저입니다.
+        <br />
+        운영진에게 문의해주세요.
+      </StyledTextContainer>
+      <Button onClick={handleClickLogin}>다시 로그인하기</Button>
+    </StyledAuthErrorPageContainer>
   );
 }
+
+const StyledAuthErrorPageContainer = styled(Stack)({
+  height: "100vh",
+  justifyContent: "space-between",
+  alignItems: "center",
+  paddingTop: 20,
+});
+
+const StyledTextContainer = styled(Box)({
+  borderRadius: 2,
+  backgroundColor: theme.palette.gray5,
+  fontWeight: 700,
+  padding: 3,
+});
