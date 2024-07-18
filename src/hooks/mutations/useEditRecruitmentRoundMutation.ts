30f44a76dd110a5b421345c406fa35c9237f0510
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-18 13:24:35 +0900

    refactor: 모집 회차 수정 api mutation 훅 리팩토링

--- src/hooks/mutations/useEditRecruitmentRoundMutation.ts
+++ src/hooks/mutations/useEditRecruitmentRoundMutation.ts
@@ -1,24 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import { recruitmentApi } from "@/apis/recruitmentApi";
-import { SemesterVariantType } from "@/types/entities/recruitment";
+import { RoundVariantType, SemesterVariantType } from "@/types/entities/recruitment";
+
+export type EditRecruitmentRoundMutationArgumentType = {
+  recruitmentRoundId: number;
+  body: {
+    academicYear: number;
+    semesterType: SemesterVariantType;
+    name: string;
+    startDate: string;
+    endDate: string;
+    roundType: RoundVariantType;
+  };
+};
 
 export default function useEditRecruitmentRoundMutation() {
   return useMutation({
-    mutationFn: ({
-      recruitmentRoundId,
-      body,
-    }: {
-      recruitmentRoundId: number;
-      body: {
-        academicYear: number;
-        semesterType: SemesterVariantType;
-        name: string;
-        startDate: string;
-        endDate: string;
-        roundType: "FIRST" | "SECOND";
-      };
-    }) => recruitmentApi.editRecruitmentRound(recruitmentRoundId, body),
+    mutationFn: ({ recruitmentRoundId, body }: EditRecruitmentRoundMutationArgumentType) =>
+      recruitmentApi.editRecruitmentRound(recruitmentRoundId, body),
     onError: (error: any) => {
       toast.error(error.response.data.errorMessage);
     },
