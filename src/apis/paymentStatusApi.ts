8577569e016d51b46ffdb2581a72efb24b89a779
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-27 20:44:50 +0900

    chore: 결제 정보 목록 조회 api 학기 검색 정보인 경우 분기 처리

--- src/apis/paymentStatusApi.ts
+++ src/apis/paymentStatusApi.ts
@@ -1,8 +1,29 @@
 import { apiClient } from ".";
+import { SearchVariantType } from "@/types/entities/payment";
 
 export const paymentStatusApi = {
-  getPaymentList: async () => {
-    const response = await apiClient.get("admin/orders");
+  getPaymentList: async (
+    page: number,
+    size: number,
+    searchVariant: SearchVariantType,
+    searchText: string,
+  ) => {
+    let url = `admin/orders?page=${page}&size=${size}`;
+
+    if (searchText && searchVariant) {
+      if (searchVariant === "semester") {
+        const [academicYear, semester] = searchText.split("-");
+        if (!semester) {
+          return;
+        }
+
+        url += `&academicYear=${academicYear}&semesterType=${semester === "1" ? "FIRST" : "SECOND"}`;
+      } else {
+        url += `&${searchVariant}=${searchText}`;
+      }
+    }
+
+    const response = await apiClient.get(url);
     return response.data;
   },
 
