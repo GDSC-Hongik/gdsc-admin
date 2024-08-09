99cd997fca670a58e86f5b024ea7a4f4870fa694
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-09 23:13:32 +0900

    chore: mkcert 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), mkcert()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
