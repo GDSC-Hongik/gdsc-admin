6d13c67a745f24532198a2829d9230749812abe3
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-10 00:44:17 +0900

    chore: 불필요한 로그인 로직 삭제

--- src/pages/AuthServerRedirectPage.tsx
+++ src/pages/AuthServerRedirectPage.tsx
@@ -1,21 +1,11 @@
 import { useEffect } from "react";
-import { Navigate, useSearchParams } from "react-router-dom";
-import { Params } from "@/constants/auth";
-import useAuthStorage from "@/hooks/auth/useAuthStorage";
+import { Navigate } from "react-router-dom";
 import RoutePath from "@/routes/routePath";
 
 export const AuthServerRedirectPage = () => {
-  const { setAuthData } = useAuthStorage();
-
-  const [searchParams] = useSearchParams();
-
-  const accessToken = searchParams.get(Params.AccessToken);
-  const refreshToken = searchParams.get(Params.AccessToken);
-
   useEffect(() => {
-    setAuthData({ accessToken, refreshToken });
     sessionStorage.setItem("isLogin", "true");
-  }, [accessToken, refreshToken, setAuthData]);
+  }, []);
 
   return <Navigate to={RoutePath.Index} />;
 };
