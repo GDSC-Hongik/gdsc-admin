a7bcdd3a06bd6ed24f99c2313b8be422dd32670b
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-11 19:43:41 +0900

    chore: 대시보드 api는 로그인 후 호출하도록 수정

--- src/pages/AllMembersPage.tsx
+++ src/pages/AllMembersPage.tsx
@@ -1,9 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { allMemberApi } from "@/apis/allMemberApi";
 import Title from "@/components/@common/Title";
 import AllMembersHeader from "@/components/AllMembers/AllMembersHeader";
 import AllMembersInfoTable from "@/components/AllMembers/AllMembersInfoTable";
 import AllMembersContextProvider from "@/contexts/AllMembersContext";
+import useLogoutMutation from "@/hooks/mutations/useLogoutMutation";
+import RoutePath from "@/routes/routePath";
 
-export default function AllMembersPage() {
+export default async function AllMembersPage() {
+  const navigate = useNavigate();
+  const { mutate } = useLogoutMutation();
+
+  const {
+    member: { manageRole },
+  } = await allMemberApi.getDashboardInfo();
+
+  if (manageRole !== "ADMIN") {
+    toast.error("어드민 권한이 없는 사용자입니다.");
+    mutate();
+    navigate(RoutePath.Signin);
+  }
+
   return (
     <>
       <Title variant={"allMember"} descriptionText={"전체 커뮤니티 멤버 정보를 관리합니다."} />
--- src/pages/AuthServerRedirectPage.tsx
+++ src/pages/AuthServerRedirectPage.tsx
@@ -1,32 +1,8 @@
-import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import { allMemberApi } from "@/apis/allMemberApi";
-import useLogoutMutation from "@/hooks/mutations/useLogoutMutation";
+import { Navigate } from "react-router-dom";
 import RoutePath from "@/routes/routePath";
 
 export const AuthServerRedirectPage = () => {
-  const navigate = useNavigate();
-  const { mutate } = useLogoutMutation();
-
-  useEffect(() => {
-    const fetchManageRoleInfo = async () => {
-      const {
-        member: { manageRole },
-      } = await allMemberApi.getDashboardInfo();
-
-      if (manageRole === "ADMIN") {
-        sessionStorage.setItem("isLogin", "true");
-        return;
-      }
-
-      toast.error("어드민 권한이 없는 사용자입니다.");
-      mutate();
-      navigate(RoutePath.Signin);
-    };
-
-    fetchManageRoleInfo();
-  }, [mutate, navigate]);
+  sessionStorage.setItem("isLogin", "true");
 
   return <Navigate to={RoutePath.Index} />;
 };
